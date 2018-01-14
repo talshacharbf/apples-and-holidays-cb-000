970b44593707f5b8f791d69e1bc7691c06459937
tal shachar <tal.shachar@buzzfeed.com>
2018-01-14 22:41:20 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   def all_supplies_in_holidays(holiday_hash)
-  holiday_hash.each do |season, holidays|
+  holiday_supplies.each do |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
       puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
