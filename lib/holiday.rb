c0c3930a82ef3c132ca1e79cc4483dbf5bb5f694
tal shachar <tal.shachar@buzzfeed.com>
2018-01-14 22:47:22 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,8 +63,8 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  holiday_hash.map do |season, holidays|
-    holidays.map do |holiday, supplies|
+  holiday_hash.each do |season, holidays|
+    holidays.each do |holiday, supplies|
       holiday if supplies.include?("BBQ")
     end
   end.flatten.compact
